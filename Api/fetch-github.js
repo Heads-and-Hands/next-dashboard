03ddf70acf6300eaa49c68f7386b2d075e50a1d6
vladmelnikov <vladislav.melnikov.98@gmail.com>
2017-12-02 18:19:30 +0300

    regular expression

--- Api/fetch-github.js
+++ Api/fetch-github.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function fetchGithub(id) {
   const token = 'f9e7ef8ece587/366908282/e834f171/f8f8478dfc';
-  const url = `https://api.github.com/repos/Heads-and-Hands/${id}?access_token=${token.replace('/', '')}`;
+  const url = `https://api.github.com/repos/Heads-and-Hands/${id}?access_token=${token.replace(/[/]/g, '')}`;
   const headers = {
     Accept: 'application/vnd.github.v3+json',
     'Content-Type': 'application/json; charset=utf-8',
