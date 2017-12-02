cfdc395715a428b86defcc20387a24d35198f9e5
vladmelnikov <vladislav.melnikov.98@gmail.com>
2017-12-02 18:06:45 +0300

    git

--- Api/fetch-github.js
+++ Api/fetch-github.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default async function fetchGithub(id) {
-  const token = 'abecdfaebe200bb42da6d9323bdda00be4d4ee33';
-  const url = `https://api.github.com/repos/Heads-and-Hands/${id}?access_token=${token}`;
+  const token = 'f9e7ef8ece587/366908282/e834f171/f8f8478dfc';
+  const url = `https://api.github.com/repos/Heads-and-Hands/${id}?access_token=${token.replace('/', '')}`;
   const headers = {
     Accept: 'application/vnd.github.v3+json',
     'Content-Type': 'application/json; charset=utf-8',
